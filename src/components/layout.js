c3c18e4acda6db33e7f57bdd3d53b3bbe5f02fed
Freddy Guerra <arcobaleno262@gmail.com>
2023-04-14 01:28:35 -0300

    first part of the Layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,24 +1,68 @@
-import React from 'react'
-import Head form 'next/Head'
+import React from "react";
+import Head from "next/head";
 import {
-    Box,
-    Flex,
-    Text,
-    Stack,
-    Button
-} from '@chakra-ui/react'
+  Box,
+  Flex,
+  Text,
+  Stack,
+  Button,
+  useColorModeValue,
+} from "@chakra-ui/react";
 
-export const layout = ({children }) => {
+export const Layout = ({ children }) => {
   return (
-    <div><head><title>E-commerce APP</title></head>
-    <Box>
-        <Flex>
-            <Flex><Text>logo</Text><Stack><Button>Sign in</Button><Button>Sign out</Button></Stack></Flex>
+    <div>
+      <Head>
+        <title>E-commerce APP</title>
+      </Head>
+      <Box>
+        <Flex
+          bg={useColorModeValue("white", "gray.600")}
+          minH={"60px"}
+          py={{ base: 2 }}
+          px={{ base: 4 }}
+          borderBottom={1}
+          borderStyle={"solid"}
+          borderColor={useColorModeValue("gray.200", "gray.900")}
+          align={"center"}
+        >
+          <Flex flex={{ base: 1 }} justify={{ base: "center", md: "start" }}>
+            <Text
+              fontFamily={"Heading"}
+              color={useColorModeValue("gray.800", "white")}
+            >
+              logo
+            </Text>
+            <Stack
+              flex={{ base: 2, md: 0 }}
+              justify={"flex-end"}
+              direction={"row"}
+              spacing={6}
+            >
+              <Button
+                as={"a"}
+                fontSize={"sm"}
+                fontWeight={400}
+                variant={"link"}
+              >
+                Sign in
+              </Button>
+              <Button
+                fontSize={"sm"}
+                fontWeight={600}
+                color={"white"}
+                bg={useColorModeValue("pink.600")}
+                variant={"link"}
+              >
+                Sign out
+              </Button>
+            </Stack>
+          </Flex>
         </Flex>
-    </Box>
-    {children}
+      </Box>
+      {children}
     </div>
-  )
-}
+  );
+};
 
-export default layout
+export default Layout;
