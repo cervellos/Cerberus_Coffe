9407ed172e7ef0c69efd9f18b2a2837b100e8567
Freddy Guerra <arcobaleno262@gmail.com>
2023-04-18 12:43:08 -0300

    Grid-Card

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,20 +1,24 @@
 import Image from "next/image";
 import React from "react";
 import { data } from "../utils/data";
+import { Grid, GirdItem, Card } from "@chakra-ui/react";
+import Link from "next/link";
 
 const Products = () => {
   return (
     <div>
-      {data.products.map((product) => (
-        <Image
-          key={product.id}
-          height={230}
-          width={230}
-          src={`/images/${product.image}`}
-          style={{ objectFit: "cover", height: "230px" }}
-          alt={product.title}
-        />
-      ))}
+      <Grid templateColumns="repeat(4, 1fr)" gap={6}>
+        {data.products.map((product) => (
+          <Image
+            key={product.id}
+            height={230}
+            width={230}
+            src={`/images/${product.image}`}
+            style={{ objectFit: "cover", height: "230px" }}
+            alt={product.title}
+          />
+        ))}
+      </Grid>
     </div>
   );
 };
