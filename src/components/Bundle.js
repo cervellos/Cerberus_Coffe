d7f386b54811108cd19a98eafccf34bc173bed7e
Freddy Guerra <arcobaleno262@gmail.com>
2023-04-27 15:37:09 -0300

    bundle ajust

--- src/components/Bundle.js
+++ src/components/Bundle.js
@@ -13,7 +13,7 @@ export const Bundle = () => {
   return (
     <Link>
       <AspectRatio
-        maxW="100%"
+        maxW="99vw"
         ratio={14 / 6}
         border={"black solid 15px"}
         paddingTop={"10px"}
@@ -21,27 +21,28 @@ export const Bundle = () => {
       >
         <Box
           backgroundImage="url('https://images.unsplash.com/photo-1495774856032-8b90bbb32b32?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80')"
-          backgroundSize="100% 100%"
-          background-repeat="no-repeat"
-          filter="brightness(60%)"
+          backgroundSize="cover"
+          backgroundRepeat="no-repeat"
+          backgroundPosition="center"
+          zIndex={4}
+          overflow={"hidden"}
+          opacity={"0.5"}
         >
           <Box
-            zIndex={-3}
-            paddingLeft={"100px"}
-            paddingRight={"100px"}
-            filter={"brightness(160%)"}
+            zIndex={1}
+            paddingLeft={"25%"}
+            paddingRight={"25%"}
+            color={"white"}
+            fontSize={"2vw"}
           >
             ¡Bienvenidos al mundo del café! En nuestra compañía, nos apasiona el
             café y nos esforzamos por brindar la mejor experiencia a nuestros
             clientes. Desde la selección cuidadosa de los granos de café hasta
             el proceso de tostado y preparación, cada taza de café que servimos
             es un testimonio de nuestro compromiso con la calidad y el sabor
-            excepcional. Nos enorgullece ofrecer una amplia variedad de opciones
-            de café y bebidas especiales, en un ambiente acogedor y relajante
-            que invita a nuestros clientes a disfrutar de una experiencia única.
-            ¡Únete a nosotros en esta aventura de café y descubre todo lo que
-            tenemos para ofrecer!
-            <Button top={50} right={800}>
+            excepcional. ¡Únete a nosotros en esta aventura de café y descubre
+            todo lo que tenemos para ofrecer!
+            <Button top={50} right={800} bg={"brand.mainColor"}>
               Conocer mas de nosotros
             </Button>
           </Box>
