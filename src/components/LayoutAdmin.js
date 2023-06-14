6aea17e33e76cd2429f7be4807cc03f5c3c220ea
Freddy Guerra <arcobaleno262@gmail.com>
2023-06-14 00:08:09 -0300

    new env

--- src/components/LayoutAdmin.js
+++ src/components/LayoutAdmin.js
@@ -9,8 +9,8 @@ export default function LayoutAdmin({ children }) {
       <div className="bg-bgGray w-screen h-screen flex items-center">
         <div className="text-center w-full">
           <button
-            onClick={() => signIn("google")}
-            className="bg-white p-2 px-4 rounded-lg"
+            onClick={() => signIn()}
+            className="bg-white text-blue-900 p-2 px-4 rounded-lg"
           >
             Login with Google
           </button>
--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -2,12 +2,25 @@ import clientPromise from "@/lib/mongodb";
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import EmailProvider from "next-auth/providers/email";
 
 export default NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
+    }),
+
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
     }),
   ],
   adapter: MongoDBAdapter(clientPromise),
