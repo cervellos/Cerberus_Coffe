d0be62653ca01672e4c83737c9b2966cdd5dc406
Freddy Guerra <arcobaleno262@gmail.com>
2023-06-16 12:32:14 -0300

    await nextauth

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -4,7 +4,7 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import EmailProvider from "next-auth/providers/email";
 
-export default NextAuth({
+export default await NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
