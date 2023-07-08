9d737d5ff79e01e9b7d2cd84a6a2bf8dc600201f
Freddy Guerra <arcobaleno262@gmail.com>
2023-07-08 12:59:08 -0300

    moongooseProductAdmin

--- models/Product.js
+++ models/Product.js
@@ -5,9 +5,9 @@ const productSchema = new mongoose.Schema(
   {
     id: { type: string, required: true, unique: true },
     name: { type: string, required: true },
-    images: { type: string, required: true },
-    price: { type: Number, required: true },
     description: { type: string, required: true },
+    price: { type: Number, required: true },
+    images: { type: string, required: true },
   },
   {
     timestamps: true,
--- src/pages/api/products.js
+++ src/pages/api/products.js
@@ -1,5 +1,19 @@
-export default function handle(req, res) {
+import mongoose from "mongoose";
+import clientPromise from "@/lib/mongodb";
+import { Product } from "@/models/Product";
+
+export default async function handle(req, res) {
   const { method } = req;
+  mongoose.Promise = clientPromise;
   if (method === "POST") {
+    const { id, name, description, price, images } = req.body;
+    const productDoc = await Product.create({
+      id,
+      name,
+      description,
+      price,
+      images,
+    });
+    res.json(productDoc);
   }
 }
